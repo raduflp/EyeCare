5c7d4b938ab5efe394ac785c55e9e950dc1e7fba
Radu <radutufilip@yahoo.com>
2014-07-10 21:37:31 +0300

    Added Chrome App specific files

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('index.html', {
+    width: 400,
+    height: 300
+  });
+});
+      
